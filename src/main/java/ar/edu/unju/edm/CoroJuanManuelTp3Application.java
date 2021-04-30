7d6d063ebd00d34446fa4d6ff1b7cd0f0adaae56
koritacrah <juanmcoro2003@gmail.com>
2021-04-30 02:37:50 -0300

    entrega tp3

---
+++ src/main/java/ar/edu/unju/edm/CoroJuanManuelTp3Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoroJuanManuelTp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoroJuanManuelTp3Application.class, args);
+	}
+
+}
